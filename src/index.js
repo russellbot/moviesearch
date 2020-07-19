e70d0f6abaaea9711c9d596c1c42b102c32d37d3
Russell Hart <russellbot@gmail.com>
2020-07-19 10:12:44 -0400

    created card components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting ={'Hello' + 'React Ninja'}/>
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
