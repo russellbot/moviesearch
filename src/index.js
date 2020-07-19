862b5a2b88c14f63232f7d9c572b6b2686ec5eb0
Russell Hart <russellbot@gmail.com>
2020-07-19 11:47:21 -0400

    added map function to loop through all users

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+// import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    </div>
+    <CardList robots={robots} />
   </React.StrictMode>,
   document.getElementById('root')
 );
