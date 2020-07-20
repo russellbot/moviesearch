01a35f968188b05ec0b2823617a813944d88a4ed
Russell Hart <russellbot@gmail.com>
2020-07-20 16:15:13 -0400

    added api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?t=star+wars&apikey=469942e0')
+            .then(response=> response.json())
+            .then(movie => this.setState({ robots: movie}));
     }
 
     onSearchChange = (event) => {
@@ -18,11 +24,11 @@ class App extends Component {
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+            return robots.Title.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
         return (
             <div className = 'tc'>
-                <h1>Movie Database Search</h1>
+                <h1 className='f1'>movie database search</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
             </div>
