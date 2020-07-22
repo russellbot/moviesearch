3625da21644d81e767c684c141d1094677ff997e
Russell Hart <russellbot@gmail.com>
2020-07-22 10:58:42 -0400

    centered title

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ title, year, id, poster }) => {
         <div className='navy bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc mh2'>
             <img className='w-50 h5' alt='poster' src={poster} />
             <div>
-                <h3 className='mw5 ma23'>{title}</h3>
+                <h3 className='mw5 ph1 mh4'>{title}</h3>
                 <p>Year: {year}</p>
             </div>
         </div>
