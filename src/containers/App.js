cb0d5832506cac7d6747f3bdd82ffbc04b07887d
Russell Hart <russellbot@gmail.com>
2020-07-23 16:22:48 -0400

    connected redux to react

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField } from '../actions.js';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            movies: [],
-            searchfield: ''
+            movies: []
         }
     }
 
@@ -20,14 +34,11 @@ class App extends Component {
             .then(movie => this.setState({ movies: movie.Search}));
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
     render() {
-        const { movies, searchfield } = this.state;
+        const { movies } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredMovies = movies.filter(movie => {
-            return movie.Title.toLowerCase().includes(searchfield.toLowerCase());
+            return movie.Title.toLowerCase().includes(searchField.toLowerCase());
         })
         if (movies.length === 0) {
             return <h1>Loading</h1>
@@ -35,7 +46,7 @@ class App extends Component {
             return (
                 <div className = 'tc'>
                     <h1 className='f1'>star wars movie search</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList movies={filteredMovies} />
@@ -47,4 +58,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
-// import Card from './Card';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchMovies } from './reducers';
+
+const store = createStore(searchMovies)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
