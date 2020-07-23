8169f6fc9b2fdef98ec9e44a3db72727637b5c46
Russell Hart <russellbot@gmail.com>
2020-07-23 18:17:51 -0400

    added redux thunk middleware

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,44 +6,38 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from '../actions.js';
+import { setSearchField, requestMovies } from '../actions.js';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchMovies.searchField,
+        movies: state.requestMovies.movies,
+        isPending: state.requestMovies.isPending,
+        error: state.requestMovies.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestMovies: () => dispatch(requestMovies())
     }
 }
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            movies: []
-        }
-    }
-
+   
     componentDidMount() {
-        fetch('https://www.omdbapi.com/?s=star+wars&apikey=469942e0')
-            .then(response=> response.json())
-            .then(movie => this.setState({ movies: movie.Search}));
+        this.props.onRequestMovies();
     }
 
     render() {
-        const { movies } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, movies, isPending } = this.props;
         const filteredMovies = movies.filter(movie => {
             return movie.Title.toLowerCase().includes(searchField.toLowerCase());
         })
-        if (movies.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return isPending ?
+            <h1>Loading</h1> :
+            (
                 <div className = 'tc'>
                     <h1 className='f1'>star wars movie search</h1>
                     <SearchBox searchChange={onSearchChange} />
@@ -54,7 +48,7 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-        }
+        
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { searchMovies } from './reducers';
+import { searchMovies, requestMovies } from './reducers';
 
-const store = createStore(searchMovies)
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchMovies, requestMovies })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
   <React.StrictMode>
