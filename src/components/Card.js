1573b1e0c25c78f1b88414c4cd3240ac606a26f5
Russell Hart <russellbot@gmail.com>
2020-07-30 17:56:58 -0400

    changed some css

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ title, year, id, poster }) => {
     return (
-        <div className='navy bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc mh2'>
+        <div id='card' className='navy bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc mh2 h7'>
             <img className='w-50 h5' alt='poster' src={poster} />
             <div>
                 <h2 className='mw5 ph1 mh4'>{title}</h2>
