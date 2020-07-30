e4ea5c9d3c6566bab4cf59cb506c24e75e65cb47
Russell Hart <russellbot@gmail.com>
2020-07-30 17:59:43 -0400

    fixed css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ title, year, id, poster }) => {
     return (
         <div id='card' className='navy bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc mh2 h7'>
-            <img className='w-50 h5' alt='poster' src={poster} />
+            <img className='h5' alt='poster' src={poster} />
             <div>
                 <h2 className='mw5 ph1 mh4'>{title}</h2>
                 <p>Year: {year}</p>
