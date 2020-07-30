6c942e8269523ea28342315d540ca7436d37a800
Russell Hart <russellbot@gmail.com>
2020-07-30 17:46:33 -0400

    fixed issues on lighthouse report

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ title, year, id, poster }) => {
         <div className='navy bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc mh2'>
             <img className='w-50 h5' alt='poster' src={poster} />
             <div>
-                <h3 className='mw5 ph1 mh4'>{title}</h3>
+                <h2 className='mw5 ph1 mh4'>{title}</h2>
                 <p>Year: {year}</p>
             </div>
         </div>
