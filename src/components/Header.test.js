97860dc8c4c944fb679051b3129a93ef0637a43e
Russell Hart <russellbot@gmail.com>
2020-08-04 21:49:51 -0400

    added test to Header.test.js

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -5,3 +5,9 @@ import Header from './Header';
 it('expect to render Header component', () => {
     expect(shallow(<Header />)).toMatchSnapshot();
 })
+
+let wrapper;
+it('renders MainPage without crashing', () => {
+    wrapper = shallow(<Header />)    
+    expect(wrapper.instance().shouldComponentUpdate()).toEqual(false);
+}) 
