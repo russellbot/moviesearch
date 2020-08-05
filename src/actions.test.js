727b7552d7d797153df1aeed36c01001ce4108dd
Russell Hart <russellbot@gmail.com>
2020-08-05 15:52:37 -0400

    added action tests

--- src/actions.test.js
+++ src/actions.test.js
@@ -5,13 +5,13 @@ import {
     REQUEST_MOVIES_SUCCESS,
     REQUEST_MOVIES_FAILED 
 } from './constants.js';
+import { shallow } from 'enzyme';
+import nock from 'nock';
 
 import configureMockStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk';
 
 const mockStore = configureMockStore([thunkMiddleware])
-
-const nock = require('nock')
 
 it('should create an action to search robots', () => {
     const text = 'of the Jedi';
@@ -22,32 +22,32 @@ it('should create an action to search robots', () => {
     expect(actions.setSearchField(text)).toEqual(expectedAction)
 })
 
+describe('Fetch movies pending action', () => {
+    it('should create a Pending action on request movies', () => {
+        const store = mockStore();
+        store.dispatch(actions.requestMovies())
+        const action = store.getActions();
+        expect(action[0]).toEqual({type: "REQUEST_MOVIES_PENDING"})    
+})
+})
 
-it('handles requesting movies API', () => {
-    const store = mockStore();
-    store.dispatch(actions.requestMovies())
-    const action = store.getActions();
-    const expectedAction = {
-        type: REQUEST_MOVIES_PENDING
-    }
-
-    // setting up mock fetch
-    const scope = nock('https://www.omdbapi.com').get('/?s=star+wars&apikey=469942e0').reply(200, {
-        Search: [{
-            Title: "Star Wars: Episode IV - A New Hope",
-            Year: "1977",
-            imdbID: "tt0076759",
-            Type: "movie",
-            Poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        }]
+describe('Fetch movies successful action', () => {
+    it('should create a SUCCESS action on successful fetch request', () =>{
+        const store = mockStore();
+        store.dispatch(actions.requestMovies())
+        const action = store.getActions();
+        // setting up mock fetch
+        const scope = nock('https://www.omdbapi.com').get('/?s=star+wars&apikey=469942e0').reply(200, {
+            Search: [{
+                Title: "Star Wars: Episode IV - A New Hope",
+                Year: "1977",
+                imdbID: "tt0076759",
+                Type: "movie",
+                Poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+            }]
+        })
+        
+        //check that the mock fetch request matches
+        expect(scope.interceptors[0].body).toEqual("{\"Search\":[{\"Title\":\"Star Wars: Episode IV - A New Hope\",\"Year\":\"1977\",\"imdbID\":\"tt0076759\",\"Type\":\"movie\",\"Poster\":\"https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg\"}]}")
     })    
-
-    expect(action[0]).toEqual(expectedAction)
-    // expect(scope()).toEqual({        
-    //     Title: "Star Wars: Episode IV - A New Hope",
-    //     Year: "1977",
-    //     imdbID: "tt0076759",
-    //     Type: "movie",
-    //     Poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"        
-    // })
 })
